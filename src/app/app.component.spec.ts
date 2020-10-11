7f56620884134fbae9406e3022f49f727d4c3196
michal.s <michal.shitrit@sapiens.com>
2020-10-11 18:40:59 +0300

    remove tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+/*  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
@@ -26,7 +26,7 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('my-new-angular-app');
   });
 
- /* it('should render title', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
