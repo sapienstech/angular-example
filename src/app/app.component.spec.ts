3e832b841c062d1da5f69b91ce2824adb554d2e7
urielTwito <uriel.t@sapiens.com>
2020-10-12 15:42:25 +0300

    test 1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,6 +17,7 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+    app.sayHello();
     expect(app).toBeTruthy();
   });
 
