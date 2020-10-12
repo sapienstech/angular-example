1ed6a8a17d69e882c50465405b7f46702672c8e4
michal.s <michal.shitrit@sapiens.com>
2020-10-12 12:39:05 +0300

    remove tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,10 +8,10 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
- /* it('should display welcome message', () => {
+/* it('should display welcome message', () => {
     page.navigateTo();
     expect(page.getTitleText()).toEqual('my-new-angular-app app is running!');
-  });*/
+  });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
@@ -19,5 +19,5 @@ describe('workspace-project App', () => {
     expect(logs).not.toContain(jasmine.objectContaining({
       level: logging.Level.SEVERE,
     } as logging.Entry));
-  });
+  });*/
 });
